4be55ab5ea4fbbb32a4fc3f2c2b921a6b014fa33
cuixiaohui233 <3176054945@qq.com>
2018-06-13 20:07:25 +0800

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,74 +1,51 @@
-pragma solidity ^0.4.18;
+const Web3 = require('web3');
 
-contract Orders {
+let web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+//输出初始化结果
+// console.log('Initialization web3 complete,the first account is '+ web3.eth.accounts[1]);
+let fs = require('fs');
+let code = fs.readFileSync('Orders.sol').toString();
+let solc = require('solc');
+//编译合约为ABI文件
+let compiledCode = solc.compile(code);
 
-	struct Order{
-    uint uin;
-		string name;
-		uint time;
-  }
+console.log(compiledCode)
+//部署合约至区块链节点
+let abiDefinition = JSON.parse(compiledCode.contracts[':Orders'].interface);
+//写入ABI文件至本地文件目录
+fs.writeFile('Orders.json',JSON.stringify(abiDefinition), {}, function(err) {
+// console.log('write ABI file [Orders.json] complete . ');
+});
 
-	mapping (uint => Order) public orders;
+let VotingContract = web3.eth.contract(abiDefinition);
+let byteCode = compiledCode.contracts[':Orders'].bytecode;
+//调用VotingContract对象的new()方法来将投票合约部署到区块链。new()方法参数列表应当与合约的 构造函数要求相一致。对于投票合约而言，new()方法的第一个参数是候选人名单。
+console.log(web3.eth.accounts[0]);
+let deployedContract = VotingContract.new(null,{data: byteCode, from: '0xa7d4831d700991304d690a078c6e65c5c281492c', gas: 4700000});
+//输出合约 地址，如果此处没有返回地址，可以在Ganache日志中查看到
+// console.log(deployedContract);
+setTimeout(function(){
+// console.log('deploy complete,deploy address is '+ deployedContract.address);
+let contractInstance = VotingContract.at(deployedContract.address);
+// let contractInstance = VotingContract.at('0xb3e2957f9aa802a6287ef067e91d76eada7d6322');
 
-	uint nowTime;
-	uint timeLimit = 48*60*60;
+// console.log(contractInstance);
+//测试合约调用
+// console.log(contractInstance.getAddress())
+// console.log(contractInstance.isChange(1529265844))
+// contractInstance.writeOrder(37249608,'cxh',1528878748,{from: web3.eth.accounts[0]});// 1528878739
+// contractInstance.writeOrder(37249607,'hxc',1528878739,{from: web3.eth.accounts[0]});// 1528878739
+// console.log('写入的数据是', contractInstance.writeOrder(37249608,'cxh',1528878739,{from: web3.eth.accounts[0]}));
+// console.log('合约账号',web3.eth.accounts[0]);
+// contractInstance.writeOrder(37249608,'Jack',{from: web3.eth.accounts[0]});
+console.log(contractInstance.readOrder.call(37249607));
+// console.log(contractInstance.isOwner());
+// console.log(contractInstance.writeOrder(1234));
+// console.log(contractInstance.readOrder('hh'));
+// console.log(contractInstance.writeOrder(0xd47c8fe9b305b31129bd381b315ca6a8aa771e11));
+// console.log(contractInstance.isOwner());
+// console.log(contractInstance.isChange(1527869486));
+// contractInstance.WriteOrders(1541735128, '大师姐', {from: web3.eth.accounts[0]});
+// contractInstance.getOrders.call(0);
 
-  // 在合约部署完成后，需要修改，用 getAddress 方法
-  address address1 = 0xa7d4831d700991304d690a078c6e65c5c281492c;// 用户
-	address address2 = getAddress();// owner: 0xa7d4831d700991304d690a078c6e65c5c281492c
-
-	// 判断是否超过了两天
-	function isChange(uint ctime) constant returns (bool) {
-		nowTime = block.timestamp;
-		if (nowTime - ctime < timeLimit) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	function isOwner() constant returns (bool) {
-		return address1 == address2;
-	}
-
-	function getAddress() constant returns (address) {
-		return msg.sender;
-	}
-
-	// 写操作
-  function writeOrder(uint _uin, string _name, uint _time){
-		if (isOwner()) {
-			// 管理员，可不限时间修改
-			orders[_uin] = Order({
-        uin: _uin,
-        name: _name,
-        time: _time
-      });
-		} else {
-			// 普通用户，在两天之内可修改，并且只可修改合约是否生效状态
-			if (isChange(_time)){
-				// 没有超过两天
-				orders[_uin] = Order({
-					uin: _uin,
-	        name: _name,
-	        time: _time
-	      });
-			} else {
-				// 超过两天不可以修改
-				orders[_uin] = Order({uin:123456, name:'hhh',time:1532453});
-			}
-		}
-
-	}
-
-	// 读操作
-  function readOrder(uint uin) public returns (uint, string, uint) {
-		/* if (isOwner()) { */
-			return (orders[uin].uin,orders[uin].name,orders[uin].time);
-		/* } else {
-			return orders[uin];
-		} */
-
-  }
-
-}
+},1000);
