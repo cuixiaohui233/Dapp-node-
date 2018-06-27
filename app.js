205629697a2baca232d96a4415d3bfd94bc56685
cuixiaohui233 <3176054945@qq.com>
2018-06-28 00:45:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const Web3 = require('web3');
-const contract = require('truffle-contract');
 const urlModule = require('url');
 const Tx = require('ethereumjs-tx');
 const config = require('./config.js');
+var Raven = require('raven');
+var log4js = require('log4js');
 
 if (typeof web3 !== 'undefined') {
   console.log('instance already exists');
   web3 = new Web3(web3.currentProvider);
 } else {
   console.log('new instance');
-    // 连接 infura 的远程节点，这里设置的是 rinkbey 测试网络
+    // 这里设置的是 rinkbey 测试网络
   web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/" + config.key));
 }
 
-// 智能合约的 ABI
 const ContractABI =  [
   {
     "constant": true,
@@ -194,9 +194,9 @@ const ContractABI =  [
   }
 ];
 
-// 连接到智能合约，生成合约实例
 var ContractInstance =  web3.eth.contract(ContractABI).at(config.ContractAddress);
-
+var numbernon= web3.eth.getTransactionCount(config.account, 'latest');
+numbernon--;
 const app = express();
 // 设置跨域访问
 app.all('*', function(req, res, next) {
@@ -205,11 +205,12 @@ app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
   res.header('X-Powered-By', ' 3.2.1')
   res.header('Content-Type', 'application/json;charset=utf-8')
-  next()
+  next();
 })
 // 写入以太坊
 app.get('/writeToEthereum',function(req, res){
-  console.log('开始请求');
+  console.log('Start asking for data');
+  // 处理参数
   let params = urlModule.parse(req.url, true).query;//解析数据 获得Json对象
   let order_id = parseInt(params.order_id);
   let name = params.name;
@@ -218,16 +219,16 @@ app.get('/writeToEthereum',function(req, res){
   let price = parseInt(params.price);
   let lease_time = parseInt(params.lease_time);
   let sign = params.sign;
+  // 处理 nonce
+  numbernon++;
+  console.log("Current nonce is ", numbernon);
 
-
-  var numbernon=  web3.eth.getTransactionCount(config.account) ;
+  // 处理签名合约的参数
   var nonceHex = web3.toHex(numbernon);
   var gasPrice = web3.eth.gasPrice;
   var gasPriceHex = web3.toHex(gasPrice);
   var gasLimitHex = web3.toHex(3000000);
 
-
-  // 生成合约的签名信息
   var rawTx = {
       from: config.account,
       nonce: nonceHex,
@@ -237,27 +238,41 @@ app.get('/writeToEthereum',function(req, res){
       value: '0x00',
       data: ContractInstance.writeOrder.getData(order_id, name, ctime, phone, price, lease_time, sign)
   }
-  console.log(rawTx)
-  // 新建合约签名
   var tx = new Tx(rawTx);
-  // 连接到钱包私钥地址
   var privateKey = new Buffer(config.privateKey, 'hex');
   tx.sign(privateKey);
   var serializedTx = '0x' + tx.serialize().toString('hex');
-  // 发起交易
-  web3.eth.sendRawTransaction(serializedTx, function(err, hash) {
+  web3.eth.sendRawTransaction(serializedTx, function(err, txHash) {
+    console.log("The transaction Hash value is ", txHash);
     if (!err) {
-      console.log(hash);
-      res.status(200);
-      res.json(hash);
+      setTimeout(function(){
+        web3.eth.getTransactionReceipt(txHash, function(err, receipt) {
+          if (err) {
+            res.status(200);
+            res.json({'transactionHash': txHash, 'msg': err, 'balance': null});
+          }
+          if (receipt) {
+            if (receipt.status == '0x1') {
+              console.log('Write to success,The details are ' + "\n", receipt);
+              let balance = web3.eth.getBalance(receipt.from).toNumber();
+              res.status(200);
+              res.json({'transactionHash': txHash, 'msg': 'Success', 'balance': balance});
+            } else {
+              console.log('Write to Fail,The details are ', receipt);
+              res.status(400);
+              res.json({'transactionHash': txHash, 'msg': 'Fail', 'balance': balance});
+            }
+          }
+        });
+      },60000);
     } else {
       console.log(err);
-      res.status(404);
-      res.json(err);
+      res.status(400);
+      res.json({'transactionHash': null, 'msg': 'Fail', 'balance': null});
     }
   })
 });
-// 读取以太坊数据，直接用 web.eth.call 的方法调用
+// 读取以太坊数据
 app.get('/readForEthereum',function(req, res){
   let params = urlModule.parse(req.url, true).query;
   let order_id = parseInt(params.order_id);
