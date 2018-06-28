d970d8d483604b56d7587dc2eb4957129d967288
cuixiaohui233 <3176054945@qq.com>
2018-06-28 12:59:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,203 +1,37 @@
 const express = require('express');
 const Web3 = require('web3');
+const contract = require('truffle-contract');
 const urlModule = require('url');
 const Tx = require('ethereumjs-tx');
 const config = require('./config.js');
-var Raven = require('raven');
-var log4js = require('log4js');
+const ContractABI = require('./ContractABI.js');
+const logger = require('./log4js_helper.js');
+const Raven = require('raven');
+
+// Raven配置
+Raven.config(config.raven).install();
 
 if (typeof web3 !== 'undefined') {
-  console.log('instance already exists');
+  logger.writeInfo('Web3 instance already exists');
   web3 = new Web3(web3.currentProvider);
 } else {
-  console.log('new instance');
+  logger.writeInfo('new Web3 instance');
     // 这里设置的是 rinkbey 测试网络
   web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/" + config.key));
 }
-
-const ContractABI =  [
-  {
-    "constant": true,
-    "inputs": [
-      {
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "Orderlist",
-    "outputs": [
-      {
-        "name": "uin",
-        "type": "uint48"
-      },
-      {
-        "name": "name",
-        "type": "string"
-      },
-      {
-        "name": "ctime",
-        "type": "uint48"
-      },
-      {
-        "name": "phone",
-        "type": "uint48"
-      },
-      {
-        "name": "price",
-        "type": "uint48"
-      },
-      {
-        "name": "lease_time",
-        "type": "uint48"
-      },
-      {
-        "name": "sign",
-        "type": "string"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "isOwner",
-    "outputs": [
-      {
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "getAddress",
-    "outputs": [
-      {
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": false,
-    "inputs": [
-      {
-        "name": "order_id",
-        "type": "uint48"
-      },
-      {
-        "name": "user_name",
-        "type": "string"
-      },
-      {
-        "name": "ctime",
-        "type": "uint48"
-      },
-      {
-        "name": "phone",
-        "type": "uint48"
-      },
-      {
-        "name": "price",
-        "type": "uint48"
-      },
-      {
-        "name": "lease_time",
-        "type": "uint48"
-      },
-      {
-        "name": "sign",
-        "type": "string"
-      }
-    ],
-    "name": "writeOrder",
-    "outputs": [
-      {
-        "name": "rusult",
-        "type": "string"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [
-      {
-        "name": "order_id",
-        "type": "uint48"
-      }
-    ],
-    "name": "getIndex",
-    "outputs": [
-      {
-        "name": "index",
-        "type": "uint48"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [
-      {
-        "name": "order_id",
-        "type": "uint48"
-      }
-    ],
-    "name": "readOrder",
-    "outputs": [
-      {
-        "name": "",
-        "type": "uint48"
-      },
-      {
-        "name": "",
-        "type": "string"
-      },
-      {
-        "name": "",
-        "type": "uint48"
-      },
-      {
-        "name": "",
-        "type": "uint48"
-      },
-      {
-        "name": "",
-        "type": "uint48"
-      },
-      {
-        "name": "",
-        "type": "uint48"
-      },
-      {
-        "name": "",
-        "type": "string"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  }
-];
 
 var ContractInstance =  web3.eth.contract(ContractABI).at(config.ContractAddress);
 var numbernon= web3.eth.getTransactionCount(config.account, 'latest');
 numbernon--;
 const app = express();
+// 初始化中间件
+app.use(Raven.requestHandler());
+// console.log(sdfasf);
+// 开启报警机制,线上环境Raven错误捕获及发送通知
+if (process.env.NODE_ENV === 'production') {
+    app.use(Raven.errorHandler());
+}
+
 // 设置跨域访问
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
@@ -209,7 +43,7 @@ app.all('*', function(req, res, next) {
 })
 // 写入以太坊
 app.get('/writeToEthereum',function(req, res){
-  console.log('Start asking for data');
+  logger.writeInfo('Start asking for data');
   // 处理参数
   let params = urlModule.parse(req.url, true).query;//解析数据 获得Json对象
   let order_id = parseInt(params.order_id);
@@ -221,7 +55,7 @@ app.get('/writeToEthereum',function(req, res){
   let sign = params.sign;
   // 处理 nonce
   numbernon++;
-  console.log("Current nonce is ", numbernon);
+  logger.writeInfo("Current nonce is ", numbernon);
 
   // 处理签名合约的参数
   var nonceHex = web3.toHex(numbernon);
@@ -243,22 +77,23 @@ app.get('/writeToEthereum',function(req, res){
   tx.sign(privateKey);
   var serializedTx = '0x' + tx.serialize().toString('hex');
   web3.eth.sendRawTransaction(serializedTx, function(err, txHash) {
-    console.log("The transaction Hash value is ", txHash);
+    logger.writeInfo("The transaction Hash value is " + txHash + ' You can read more information by https://rinkeby.etherscan.io');
     if (!err) {
       setTimeout(function(){
         web3.eth.getTransactionReceipt(txHash, function(err, receipt) {
           if (err) {
+            logger.writeError(err);
             res.status(200);
             res.json({'transactionHash': txHash, 'msg': err, 'balance': null});
           }
           if (receipt) {
             if (receipt.status == '0x1') {
-              console.log('Write to success,The details are ' + "\n", receipt);
+              logger.writeInfo('Write to success,The details are ' + '\n', receipt);
               let balance = web3.eth.getBalance(receipt.from).toNumber();
               res.status(200);
               res.json({'transactionHash': txHash, 'msg': 'Success', 'balance': balance});
             } else {
-              console.log('Write to Fail,The details are ', receipt);
+              logger.writeError('Write to Fail,The details are ', receipt);
               res.status(400);
               res.json({'transactionHash': txHash, 'msg': 'Fail', 'balance': balance});
             }
@@ -266,7 +101,7 @@ app.get('/writeToEthereum',function(req, res){
         });
       },60000);
     } else {
-      console.log(err);
+      logger.writeError(err);
       res.status(400);
       res.json({'transactionHash': null, 'msg': 'Fail', 'balance': null});
     }
@@ -277,6 +112,7 @@ app.get('/readForEthereum',function(req, res){
   let params = urlModule.parse(req.url, true).query;
   let order_id = parseInt(params.order_id);
   let result = ContractInstance.readOrder.call(order_id);
+  logger.writeInfo('Read success, information is ' + '\n', result);
   res.status(200);
   res.json(result);
 })
@@ -285,5 +121,5 @@ app.get('/readForEthereum',function(req, res){
 const server = app.listen(3008, function() {
   var host = server.address().address
   var port = server.address().port
-  console.log('server is listening at http://%s:%s', host, port)
+  logger.writeInfo('server is listening at http://%s:%s', host, port)
 })
