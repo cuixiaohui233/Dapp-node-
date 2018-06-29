5954ab19689dfce8a5eb4e36fa3bed5a071b4c88
cuixiaohui233 <3176054945@qq.com>
2018-06-29 22:56:47 +0800

    Update Orders.sol

--- Orders.sol
+++ Orders.sol
@@ -1,31 +1,22 @@
-pragma solidity ^0.4.18;
-
+pragma solidity ^0.4.24;
 contract Orders {
 
 	struct Order{
-    	uint8 uin;
+		uint48 uin;
 		string name;
-		uint16 time;
+		uint48 ctime;
+		uint48 phone;
+		uint48 price;
+		uint48 lease_time;
+		string sign;
   }
 
-	mapping (uint8 => Order) public orders;
+	Order[] public Orderlist;
 
-	uint16 nowTime;
-	uint16 timeLimit = uint16(172800);//48*60*60;
 
   // 在合约部署完成后，需要修改，用 getAddress 方法
-  	address owenr = 0xa7d4831d700991304d690a078c6e1492c;// 用户
-	address user = getAddress();// owner: 0xa7d4831d700991304d690a078c6e65c5c281492c
-
-	// 判断是否超过了两天
-	function isChange(uint16 ctime) public returns (bool) {
-		nowTime = uint16(block.timestamp);
-		if (nowTime - ctime < timeLimit) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+  address owenr = 0xd56D92bD315800eA70E66FdA21B0778b7d4d191F;// 用户
+  address user = getAddress();
 
 	function isOwner() public view returns (bool) {
 		return owenr == user;
@@ -34,41 +25,47 @@ contract Orders {
 	function getAddress() public view returns (address) {
 		return msg.sender;
 	}
-
+	
+    event mylog(uint48 order_id, string user_name, uint48 ctime, uint48 phone, uint48 price, uint48 lease_time, string sign);
+    
 	// 写操作
-  function writeOrder(uint8 order_id, string user_name, uint16 ctime) public{
+  function writeOrder(uint48 order_id, string user_name, uint48 ctime, uint48 phone, uint48 price, uint48 lease_time, string sign) public  returns (string rusult){
 		if (isOwner()) {
 			// 管理员，可不限时间修改
-			orders[order_id] = Order({
-            uin: order_id,
-            name: user_name,
-            time: ctime
-          });
+			Orderlist.push(Order({
+				uin: order_id,
+				name: user_name,
+				ctime: ctime,
+				phone: phone,
+				price: price,
+				lease_time: lease_time,
+				sign: sign
+			}));
+		  	return '写入成功';
 		} else {
-			// 普通用户，在两天之内可修改，并且只可修改合约是否生效状态
-			if (isChange(ctime)){
-				// 没有超过两天
-				orders[order_id] = Order({
-					uin: order_id,
-        	        name: user_name,
-        	        time: ctime
-	                });
-			} else {
-				// 超过两天不可以修改
-				orders[order_id] = Order({uin:uint8(1234), name:'hhh',time:uint16(1532453)});
-			}
+		  return '您暂时没有此权限';
 		}
 
 	}
-
+    function getIndex(uint48 order_id) public view returns (uint48 index){
+        uint48 num;
+        for(uint8 i=0;i<Orderlist.length;i++){
+            if (Orderlist[i].uin == order_id) {
+                num = i;
+            } else {
+                num = 0;
+            }
+        }
+        return num;
+    }
 	// 读操作
-	function readOrder(uint8 uin) internal view returns (uint8, string, uint16) {
-		/* if (isOwner()) { */
-			return (orders[uin].uin,orders[uin].name,orders[uin].time);
-		/* } else {
-			return orders[uin];
-		} */
-
+	function readOrder(uint48 order_id) public view returns (uint48, string, uint48, uint48, uint48, uint48, string) {
+        if (Orderlist.length > 0){
+            Order storage result = Orderlist[getIndex(order_id)];
+            return (result.uin, result.name, result.ctime, result.phone, result.price, result.lease_time, result.sign);
+        } else {
+            return (123456, '0000', 123456, 12345, 123456, 123456, 'xxx');
+        }
 	}
 
 }
